507a499a5f685f8a79da3efca72f3752bdb26fdd
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-08-08 15:25:39 +0200

    paragraph changed

--- src/common/Paragraph/styled.js
+++ src/common/Paragraph/styled.js
@@ -1,10 +1,19 @@
 import { styled } from "styled-components";
 
 export const StyledParagraph = styled.p`
-  margin: 20px auto;
+  margin: 0px auto;
   width: 80%;
   background-color: ${({ theme }) => theme.colors.pinkFlare};
   padding: 40px;
+ 
+  box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.3);
   font-size: 20px;
   //font-weight:700;
-  line-height: 140%;`
+  line-height: 140%;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoints.phoneMedia}px) {
+    padding:10px;
+    text-align:center;
+    width:90%;
+  }
+  `
