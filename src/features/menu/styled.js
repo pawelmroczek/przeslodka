9416550c67649945652229de7fb77bb0d8c032dc
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-08-31 11:12:00 +0200

    rwd fixed

--- src/features/menu/styled.js
+++ src/features/menu/styled.js
@@ -2,8 +2,16 @@ import { styled } from "styled-components";
 
 export const Container = styled.div`
   display:flex;
+
+  @media(max-width:${({theme})=>theme.breakpoints.phoneMedia}px){
+    flex-direction:column;
+  }
 `
 
 export const StyledPage = styled.img`
   width:50%;
+
+  @media(max-width:${({theme})=>theme.breakpoints.phoneMedia}px){
+    width:100%;
+  }
 `
