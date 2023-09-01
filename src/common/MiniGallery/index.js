d0e63dfd9ecdedcfea65115b9a7262cc29cd8780
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-09-01 09:21:11 +0200

    order of elements changed

--- src/common/MiniGallery/index.js
+++ src/common/MiniGallery/index.js
@@ -1,20 +1,18 @@
 import { Container, StyledParagraph, StyledSection } from "./styled";
-import image from "../../images/IMG_8702.jpg"
-import slodki from "../../images/ciastka14.jpg"
-import babeczki from "../../images/babeczki1.jpg"
+import image from "../../images/IMG_8702.jpg";
+import slodki from "../../images/ciastka14.jpg";
+import babeczki from "../../images/babeczki1.jpg";
 
 import Tile from "./Tile";
 import TitleWithLines from "../TitleWithLines";
 const MiniGallery = ({ title, text, content }) => (
   <StyledSection>
-    <TitleWithLines title={title}/>
-    <StyledParagraph>
-      {text}
-    </StyledParagraph>
+    <TitleWithLines title={title} />
+    <StyledParagraph>{text}</StyledParagraph>
     <Container>
-      <Tile title={"Torty"} image={image}/>
-      <Tile title={"Ciasta"} image={babeczki}/>
-      <Tile title={"Słodki Stół"} image={slodki}/>
+      <Tile title={"Torty"} image={image} />
+      <Tile title={"Słodki Stół"} image={slodki} />
+      <Tile title={"Inne słodkości"} image={babeczki} />
     </Container>
   </StyledSection>
 );
