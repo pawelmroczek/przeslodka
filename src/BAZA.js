e2d1ebb7589ec08154cc5dedaf203b4d5dc8b0fe
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-09-03 11:43:39 +0200

    added things to BAZA

--- src/BAZA.js
+++ src/BAZA.js
@@ -21,59 +21,131 @@ export const BAZA = {
   torty: [
     {
       plik: "IMG_8702.jpg",
-      tytuł: "Tort z kwiatami",
+      tytuł: "Tort z kwiatami1",
+      metaTITLE: "Tort z kwiatami - Przesłodka.pl",
+      metaDESC:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam ",
       opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rutrum. Ut feugiat neque quis auctor rutrum.",
-      kategorie:["torty weselne"],
-      id:"1"
+      podtytuł1: "Lorem",
+      dodatkowyOpis1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      podtytuł2: "Ipsum",
+      dodatkowyOpis2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      kategorie: ["torty weselne"],
+      id: "1",
     },
     {
       plik: "IMG_8702.jpg",
-      tytuł: "Tort z kwiatami",
+      tytuł: "Tort z kwiatami2",
+      metaTITLE: "Tort z kwiatami - Przesłodka.pl",
+      metaDESC:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam ",
       opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rutrum. Ut feugiat neque quis auctor rutrum.",
-      kategorie:["torty na roczek"],
-      id:"2"
+      podtytuł1: "Lorem",
+      dodatkowyOpis1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      podtytuł2: "Ipsum",
+      dodatkowyOpis2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      kategorie: ["torty na roczek"],
+      id: "2",
     },
     {
       plik: "IMG_8702.jpg",
-      tytuł: "Tort z kwiatami",
+      tytuł: "Tort z kwiatami3",
+      metaTITLE: "Tort z kwiatami - Przesłodka.pl",
+      metaDESC:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam ",
       opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rutrum. Ut feugiat neque quis auctor rutrum.",
+      podtytuł1: "Lorem",
+      dodatkowyOpis1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      podtytuł2: "Ipsum",
+      dodatkowyOpis2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
       kategorie: ["torty na imprezy firmowe"],
-      id:"3"
+      id: "3",
     },
     {
       plik: "IMG_8702.jpg",
-      tytuł: "Tort z kwiatami",
+      tytuł: "Tort z kwiatami4",
+      metaTITLE: "Tort z kwiatami - Przesłodka.pl",
+      metaDESC:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam ",
       opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rutrum. Ut feugiat neque quis auctor rutrum.",
-      kategorie:["torty na roczek"],
-      id:"4"
+      podtytuł1: "Lorem",
+      dodatkowyOpis1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      podtytuł2: "Ipsum",
+      dodatkowyOpis2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      kategorie: ["torty na roczek"],
+      id: "4",
     },
     {
       plik: "IMG_8702.jpg",
-      tytuł: "Tort z kwiatami",
+      tytuł: "Tort z kwiatami5",
+      metaTITLE: "Tort z kwiatami - Przesłodka.pl",
+      metaDESC:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam ",
       opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rutrum. Ut feugiat neque quis auctor rutrum.",
-      kategorie:["torty na roczek"],
-      id:"5"
+      podtytuł1: "Lorem",
+      dodatkowyOpis1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      podtytuł2: "Ipsum",
+      dodatkowyOpis2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      kategorie: ["torty na roczek"],
+      id: "5",
     },
     {
       plik: "IMG_8702.jpg",
-      tytuł: "Tort z kwiatami",
+      tytuł: "Tort z kwiatami6",
+      metaTITLE: "Tort z kwiatami - Przesłodka.pl",
+      metaDESC:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam ",
       opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rutrum. Ut feugiat neque quis auctor rutrum.",
-      kategorie:["torty na roczek"],
-      id:"6"
+      podtytuł1: "Lorem",
+      dodatkowyOpis1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      podtytuł2: "Ipsum",
+      dodatkowyOpis2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      kategorie: ["torty na roczek"],
+      id: "6",
     },
     {
       plik: "babeczki1.jpg",
-      tytuł: "Tort z kwiatami",
+      tytuł: "Tort z kwiatami7",
+      metaTITLE: "Tort z kwiatami - Przesłodka.pl",
+      metaDESC:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam ",
       opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rutrum. Ut feugiat neque quis auctor rutrum.",
-      kategorie:["torty weselne"],
-      id:"7"
+      podtytuł1: "Lorem",
+      dodatkowyOpis1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      podtytuł2: "Ipsum",
+      dodatkowyOpis2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      kategorie: ["torty weselne"],
+      id: "7",
     },
     {
       plik: "IMG_8702.jpg",
-      tytuł: "Tort z kwiatami",
+      tytuł: "Tort z kwiatami8",
+      metaTITLE: "Tort z kwiatami - Przesłodka.pl",
+      metaDESC:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam ",
       opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rutrum. Ut feugiat neque quis auctor rutrum.",
-      kategorie:["torty weselne"],
-      id:"8"
+      podtytuł1: "Lorem",
+      dodatkowyOpis1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      podtytuł2: "Ipsum",
+      dodatkowyOpis2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies efficitur mi quis dignissim. Maecenas molestie eu quam a rut",
+      kategorie: ["torty weselne"],
+      id: "8",
     },
   ],
   strona: {
